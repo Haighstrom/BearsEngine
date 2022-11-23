6f86f97a343713d1bf5bd74433d981760aa56b0c
Haighstrom <simon.haigh@gmail.com>
2022-11-23 22:15:01 +0000

    Continued developing Tweens.

---
+++ Source/Controllers/ITimer.cs
@@ -0,0 +1,29 @@
+﻿namespace BearsEngine.Controllers;
+
+internal interface IProgressTimer : IUpdatable
+{
+    /// <summary>
+    /// The time elapsed in the timer, in seconds.
+    /// </summary>
+    public float TimeElapsed { get; }
+
+    /// <summary>
+    /// The time remaining for the timer to complete, in seconds.
+    /// </summary>
+    public float TimeRemaining { get; }
+
+    /// <summary>
+    /// The total time the timer was requested to run for.
+    /// </summary>
+    public float TotalDuration { get; }
+
+    /// <summary>
+    /// The reported progress percentage, from [0,1]. This is calculated as <see cref="TimeToProgressFunction"/>(<see cref="TimeElapsed"/>/<see cref="TotalDuration"/>) Progress is guarntee to be 0 initially, and 1 when complete, but based on <see cref="TimeToProgressFunction"/>, Progress may = 1 at other points too.
+    /// </summary>
+    public float Progress { get; }
+
+    /// <summary>
+    /// An event which triggers when the timer has completed running (TimeElapsed = TotalDuration, and TimeRemaining = 0). Note that Progress will = 1 at this point, but based on <see cref="TimeToProgressFunction"/>, Progress may = 1 at other points too.
+    /// </summary>
+    public event EventHandler? Completed;
+}
---
+++ Source/Controllers/Timer.cs
@@ -0,0 +1,83 @@
+﻿namespace BearsEngine.Controllers;
+
+/// <summary>
+/// A simple timer which reports progress
+/// </summary>
+internal class ProgressTimer : IProgressTimer
+{
+    private readonly Func<float, float> _progressFunction = t => t;
+
+    /// <summary>
+    /// A simple timer which reports progress
+    /// </summary>
+    /// <param name="totalDuration">The time in seconds that the timer should run for</param>
+    public ProgressTimer(float totalDuration)
+    {
+        TotalDuration = totalDuration;
+    }
+
+    /// <summary>
+    /// A simple timer which reports progress
+    /// </summary>
+    /// <param name="totalDuration">The time in seconds that the timer should run for</param>
+    /// <param name="progressFunction">A function mapping normalised time to reported progress. Requirements: f([0,1])->[0,1], f(0)=0 and f(1)=1. By default f(t) = t, i.e. progress is linear with time</param>
+    /// <exception cref="ArgumentException">If the progressFunction is not valid</exception>
+    public ProgressTimer(float totalDuration, Func<float, float> progressFunction)
+    {
+        if (progressFunction(0) != 0 || progressFunction(1) != 1)
+            throw new ArgumentException("time to progress mapping must meet following conditions: f([0,1])->[0,1], f(0)=0 and f(1)=1", nameof(progressFunction));
+
+        TotalDuration = totalDuration;
+        _progressFunction = progressFunction;
+    }
+
+    public bool Active { get; set; } = true;
+
+    /// <summary>
+    /// How much time, in seconds has elapsed while the timer has been active
+    /// </summary>
+    public float TimeElapsed { get; private set; } = 0;
+
+    /// <summary>
+    /// How much time, in seconds is left until the timer completes
+    /// </summary>
+    public float TimeRemaining => TotalDuration - TimeElapsed;
+
+    /// <summary>
+    /// The total duration of the timer requested.
+    /// </summary>
+    public float TotalDuration { get; }
+
+    /// <summary>
+    /// Reported progress, [0,1]. Usually normalised time, i.e. moves uniformly from 0 to 1 as TimeElapsed moves from 0 to TotalDuration, but can be modified via the constructor taking a progressFunction if non-linear progress being reported is desired.
+    /// </summary>
+    public float Progress { get; private set; }
+
+    /// <summary>
+    /// <see cref="Progress"/> multiplied by 100.
+    /// </summary>
+    public float ProgressAsPercentage => Progress * 100;
+
+    public void Update(float elapsedTime)
+    {
+        if (TimeRemaining == 0)
+            return;
+
+        TimeElapsed += elapsedTime;
+
+        if (TimeElapsed > TotalDuration)
+        {
+            TimeElapsed = TotalDuration;
+            Completed?.Invoke(this, EventArgs.Empty);
+        }
+
+        Progress = _progressFunction(TimeElapsed / TotalDuration);
+        if (Progress < 0 || Progress > 1)
+            throw new InvalidOperationException();
+    }
+
+    /// <summary>
+    /// An event which fires once <see cref="TimeElapsed"/> reaches <see cref="TotalDuration"/>. Note that Progress will be 1 at this point, but may be 1 before this event triggers, if a progressFunction was provided in the constructor.
+    /// </summary>
+    public event EventHandler? Completed;
+}
