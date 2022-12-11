6160e563b6e61a3a38c559e6f9c111bac7e36cfb
Haighstrom <simon.haigh@gmail.com>
2022-12-11 19:31:26 +0000

    Refactoring and commenting

---
+++ Source/Controllers/MotionControllers/AttachToSideController.cs
@@ -0,0 +1,61 @@
+﻿namespace BearsEngine.Source.Controllers.MotionControllers;
+
+/// <summary>
+/// A controller which will attach an object to the side of another one, and move with it
+/// </summary>
+public class AttachToSideController : AddableBase, IUpdatable
+{
+    private readonly Rect _target;
+    private readonly Direction _direction;
+    private readonly Rect _hangFrom;
+
+    /// <summary>
+    /// Create an AttachToSideController
+    /// </summary>
+    /// <param name="target">The target to be controlled</param>
+    /// <param name="directionToAttachFrom">The direction the target should attach from.</param>
+    /// <param name="hangFrom">The object the target should attach to.</param>
+    public AttachToSideController(Rect target, Direction directionToAttachFrom, Rect hangFrom)
+    {
+        _target = target;
+        _direction = directionToAttachFrom;
+        _hangFrom = hangFrom;
+
+        UpdatePosition();
+    }
+
+    /// <summary>
+    /// Whether this object should be updated
+    /// </summary>
+    public bool Active { get; set; } = true;
+
+    private void UpdatePosition()
+    {
+        switch (_direction)
+        {
+            case Direction.Up:
+                _target.Y = _hangFrom.Top - _target.H;
+                break;
+            case Direction.Right:
+                _target.X = _hangFrom.Right;
+                break;
+            case Direction.Down:
+                _target.Y = _hangFrom.Bottom;
+                break;
+            case Direction.Left:
+                _target.X = _hangFrom.Left - _target.W;
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Update this object
+    /// </summary>
+    /// <param name="elapsed">How much time should pass for the object</param>
+    public virtual void Update(float elapsed)
+    {
+        UpdatePosition();
+    }
+}
--- Source/Controllers/YIndexedController.cs
+++ Source/Controllers/YIndexedController.cs
@@ -1,10 +1,18 @@
 ﻿namespace BearsEngine.Controllers;
 
+/// <summary>
+/// A controller for automatically updating Layer in an entity as its Y position updates
+/// </summary>
 public class YIndexedLayerController : AddableBase, IUpdatable
 {
     private readonly float _baseY;
     private readonly Entity _target;
 
+    /// <summary>
+    /// Creates the controller
+    /// </summary>
+    /// <param name="target">The target to update the layer of</param>
+    /// <param name="baseY">The default Layer. the target's Y position will be sutracted from this layer.</param>
     public YIndexedLayerController(Entity target, float baseY)
     {
         _target = target;
@@ -13,10 +21,17 @@ public class YIndexedLayerController : AddableBase, IUpdatable
         _target.PositionChanged += OnTargetPositionChanged;
     }
 
+    /// <summary>
+    /// Whether this object should be updated
+    /// </summary>
     public bool Active { get; set; } = true;
 
     private void OnTargetPositionChanged(object? sender, PositionChangedEventArgs e) => _target.Layer = (int)(_baseY - e.NewR.Y);
-
+    
+    /// <summary>
+    /// Update this object
+    /// </summary>
+    /// <param name="elapsed">How much time should pass for the object</param>
     public void Update(float elapsed)
     {
     }
