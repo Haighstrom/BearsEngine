5c6e3b3384f1ec3453b4367fde41e1e81a8c475f
Haighstrom <simon.haigh@gmail.com>
2022-12-18 13:49:59 +0000

    Moved IRectangular from Haighframework to Bearsengine

---
+++ Source/Geometry/IRectangular.cs
@@ -0,0 +1,12 @@
+﻿namespace BearsEngine;
+
+public interface IRectangular : IPosition
+{
+    new float X { get; set; }
+    new float Y { get; set; }
+    float W { get; set; }
+    float H { get; set; }
+    Point P { get; set; }
+    Point Size { get; set; }
+    Rect R { get; set; }
+}
