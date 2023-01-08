4677fa6991c4e29434d22e2e3c3c449ec0fb86e7
Haighstrom <simon.haigh@gmail.com>
2023-01-08 11:32:42 +0000

    Added IList.GetRandomElement extension

---
+++ Source/Tools/Randomisation.cs
@@ -0,0 +1,9 @@
+﻿namespace BearsEngine;
+
+public static class Randomisation
+{
+    public static T RandomElement<T>(this IList<T> list)
+    {
+        return list[HF.Randomisation.Rand(list.Count)];
+    }
+}
