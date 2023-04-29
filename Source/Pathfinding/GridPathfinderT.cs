22945ea8b180989219d7c02c80fd1a1e1cccd8ed
Haighstrom <simon.haigh@gmail.com>
2023-04-29 14:01:25 +0100

    Given GridPathfinder.NodeGrid protected accessibility

--- Source/Pathfinding/GridPathfinderT.cs
+++ Source/Pathfinding/GridPathfinderT.cs
@@ -6,47 +6,38 @@ namespace BearsEngine.Pathfinding;
 /// <typeparam name="TNode">The type of Node used in this grid</typeparam>
 public class GridPathfinder<TNode> : Pathfinder<TNode>, IGridPathfinder<TNode> where TNode : IPathfindNode<TNode>, IPosition
 {
-    private readonly TNode[,] _nodegrid;
-
     public GridPathfinder(int width, int height, Func<int, int, TNode> createNode)
         : base()
     {
-        _nodegrid = new TNode[width, height];
+        Nodegrid = new TNode[width, height];
 
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
             {
-                _nodegrid[i, j] = createNode(i, j);
+                Nodegrid[i, j] = createNode(i, j);
             }
 
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
             {
                 if (IsValidGridPosition(i - 1, j))
-                    _nodegrid[i, j].ConnectedNodes.Add(_nodegrid[i - 1, j]);
+                    Nodegrid[i, j].ConnectedNodes.Add(Nodegrid[i - 1, j]);
                 if (IsValidGridPosition(i + 1, j))
-                    _nodegrid[i, j].ConnectedNodes.Add(_nodegrid[i + 1, j]);
+                    Nodegrid[i, j].ConnectedNodes.Add(Nodegrid[i + 1, j]);
                 if (IsValidGridPosition(i, j - 1))
-                    _nodegrid[i, j].ConnectedNodes.Add(_nodegrid[i, j - 1]);
+                    Nodegrid[i, j].ConnectedNodes.Add(Nodegrid[i, j - 1]);
                 if (IsValidGridPosition(i, j + 1))
-                    _nodegrid[i, j].ConnectedNodes.Add(_nodegrid[i, j + 1]);
+                    Nodegrid[i, j].ConnectedNodes.Add(Nodegrid[i, j + 1]);
             }
     }
 
-    public TNode this[int x, int y] => _nodegrid[x, y];
+    protected TNode[,] Nodegrid { get; }
 
-    public bool IsValidGridPosition(float x, float y)
-    {
-        //check x,y are integers
-        if (x % 1 != 0 || y % 1 != 0)
-            return false;
+    public TNode this[int x, int y] => Nodegrid[x, y];
 
-        return x >= 0 && x < Width && y >= 0 && y < Height;
-    }
+    public int Width => Nodegrid.GetLength(0);
 
-    public int Height => _nodegrid.GetLength(1);
-
-    public int Width => _nodegrid.GetLength(0);
+    public int Height => Nodegrid.GetLength(1);
 
     public TNode GetClosestNode(float nodeX, float nodeY)
     {
@@ -57,4 +48,13 @@ public class GridPathfinder<TNode> : Pathfinder<TNode>, IGridPathfinder<TNode> w
     }
 
     public TNode GetClosestNode(IPosition p) => GetClosestNode(p.X, p.Y);
+
+    public bool IsValidGridPosition(float x, float y)
+    {
+        //check x,y are integers
+        if (x % 1 != 0 || y % 1 != 0)
+            return false;
+
+        return x >= 0 && x < Width && y >= 0 && y < Height;
+    }
 }
