68cbb4969585dfb892965fee250ac58c7e54e2d3
Haighstrom <simon.haigh@gmail.com>
2023-04-29 18:25:19 +0100

    Introduced MoveFadeRemoveController, closes #3

---
+++ Source/Controllers/MoveFadeRemoveController.cs
@@ -0,0 +1,49 @@
+﻿namespace BearsEngine.Source.Controllers;
+
+public class MoveFadeRemoveController : UpdateableBase
+{
+    private readonly IRectGraphic _target;
+    private readonly float _speed;
+    private readonly Direction _direction;
+    private float _duration;
+
+    public MoveFadeRemoveController(IRectGraphic target, float speed, Direction direction, float duration)
+    {
+        _target = target;
+        _speed = speed;
+        _direction = direction;
+        _duration = duration;
+    }
+
+    public override void Update(float elapsed)
+    {
+        switch (_direction)
+        {
+            case Direction.Up:
+                _target.Y -= _speed * elapsed;
+                break;
+            case Direction.Right:
+                _target.X += _speed * elapsed;
+                break;
+            case Direction.Down:
+                _target.Y += _speed * elapsed;
+                break;
+            case Direction.Left:
+                _target.X -= _speed * elapsed;
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+
+        _duration -= elapsed;
+
+        if (_duration < 0)
+        {
+            _target.Remove();
+            if (_target is IDisposable disposable)
+                disposable.Dispose();
+            Remove();
+            return;
+        }
+    }
+}
