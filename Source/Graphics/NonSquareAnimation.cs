c339bbcb77f2c3c7b66f97ebe576d56213aa5afb
Haighstrom <simon.haigh@gmail.com>
2023-05-07 15:33:58 +0100

    Introduced NonSquareAnimation and NonSquareSprite to allow custom shaped spritesheets. closes #12

---
+++ Source/Graphics/NonSquareAnimation.cs
@@ -0,0 +1,99 @@
+﻿namespace BearsEngine.Graphics;
+
+public class NonSquareAnimation : NonSquareSprite, IUpdatable
+{
+    private int[] _framesToPlay = new int[] { 0 };
+    private int _playIndex = 0;
+    private LoopType _loopType;
+    private Action? _onComplete;
+
+    public NonSquareAnimation(float layer, string imgPath, float x, float y, IList<(Point OutputSize, Rect TextureSource)> frames, float animationStepTime = 0.1f)
+        : base(layer, imgPath, x, y, frames)
+    {
+        AnimStepTime = animationStepTime;
+    }
+
+    public bool Active { get; set; } = true;
+
+    public float TimeToNextFrame { get; set; }
+
+    public float AnimStepTime { get; set; }
+
+    public bool Playing { get; set; }
+
+    public int[] AllFrames => Enumerable.Range(0, TotalFrames).ToArray();
+
+    public event EventHandler? AnimationComplete;
+
+    protected void OnAnimationComplete()
+    {
+        if (_onComplete != null)
+            _onComplete();
+
+        AnimationComplete?.Invoke(this, EventArgs.Empty);
+    }
+
+    /// <summary>
+    /// Starts this animation copying another animation, including its current frame and remaining frame time
+    /// </summary>
+    /// <param name="other"></param>
+    public void PlayMatching(NonSquareAnimation other) => PlayFrom(other._loopType, other._playIndex, other.TimeToNextFrame, other._framesToPlay);
+
+    public void PlayAllOnce(Action actionOnComplete)
+    {
+        Play(LoopType.OneShot, AllFrames);
+        _onComplete = actionOnComplete;
+    }
+
+    public void PlayAllOnce() => Play(LoopType.OneShot, AllFrames);
+
+    public void PlayAllLooping() => Play(LoopType.Looping, AllFrames);
+
+    /// <summary>
+    /// Fixes the Sprite with a single frame
+    /// </summary>
+    /// <param name="frame"></param>
+    public void Play(int frame) => Play(LoopType.OneShot, frame);
+
+    public void Play(LoopType loopType, params int[] frames) => PlayFrom(loopType, 0, AnimStepTime, frames);
+
+    public void PlayOnce(Action actionOnComplete, int startFrame, params int[] frames)
+    {
+        PlayFrom(LoopType.OneShot, 0, AnimStepTime, frames);
+        _onComplete = actionOnComplete;
+    }
+
+    public void PlayFrom(LoopType loopType, int fromIndex, float currentFrameRemainingTime, params int[] frames)
+    {
+        Playing = true;
+        _framesToPlay = frames;
+        _playIndex = fromIndex;
+        TimeToNextFrame = currentFrameRemainingTime;
+        Frame = frames[fromIndex];
+        _loopType = loopType;
+    }
+
+    public virtual void Update(float elapsed)
+    {
+        if (Playing && _framesToPlay.Length > 1)
+        {
+            TimeToNextFrame -= (float)elapsed;
+            while (TimeToNextFrame <= 0)
+            {
+                if (_playIndex == _framesToPlay.Length - 1 && _loopType == LoopType.OneShot)
+                {
+                    Playing = false;
+                    TimeToNextFrame = 0;
+                    OnAnimationComplete();
+                    break;
+                }
+                else
+                {
+                    _playIndex = Maths.Mod(_playIndex + 1, _framesToPlay.Length);
+                    Frame = _framesToPlay[_playIndex];
+                    TimeToNextFrame += AnimStepTime;
+                }
+            }
+        }
+    }
+}
---
+++ Source/Graphics/NonSquareSprite.cs
@@ -0,0 +1,100 @@
+﻿using BearsEngine.Graphics.Shaders;
+using HaighFramework.OpenGL;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BearsEngine.Graphics;
+
+public class NonSquareSprite : RectGraphicBase
+{
+    private Texture _texture;
+    private Vertex[] _vertices;
+    private bool _verticesChanged = true;
+    protected int _currentFrame = 0;
+    private readonly IList<(Point OutputSize, Rect TextureSource)> _frames;
+    
+    public NonSquareSprite(float layer, string imgPath, float x, float y, IList<(Point OutputSize, Rect TextureSource)> frames, int initialFrame = 0)
+        : base(new DefaultShader(), layer, x, y, frames[initialFrame].OutputSize.X, frames[initialFrame].OutputSize.Y)
+    {
+        _frames = frames;
+        _texture = OpenGL.LoadTexture(imgPath);
+
+        Frame = initialFrame;
+
+        BindVertexBuffer();
+        SetVertices();
+        UnbindVertexBuffer();
+    }
+
+    private Rect GetTextureSource() => _frames[_currentFrame].TextureSource;
+
+    public override Colour Colour
+    {
+        set
+        {
+            base.Colour = value;
+            _verticesChanged = true;
+        }
+    }   
+
+    public int TotalFrames => _frames.Count;
+
+    public int Frame
+    {
+        get => _currentFrame;
+        set
+        {
+            if (value == _currentFrame)
+                return;
+
+            _currentFrame = Maths.Mod(value, TotalFrames);
+
+            W = _frames[_currentFrame].OutputSize.X;
+            H = _frames[_currentFrame].OutputSize.Y;
+
+            _verticesChanged = true;
+        }
+    }
+
+    [MemberNotNull(nameof(_vertices))]
+    private void SetVertices()
+    {
+        Rect texSource = GetTextureSource();
+        _vertices = new Vertex[4]
+        {
+            new Vertex(new Point(0, 0), Colour, texSource.TopLeft),
+            new Vertex(new Point(W, 0), Colour, texSource.TopRight),
+            new Vertex(new Point(0, H), Colour, texSource.BottomLeft),
+            new Vertex(new Point(W, H), Colour, texSource.BottomRight)
+        };
+
+        OpenGL.BufferData(BUFFER_TARGET.GL_ARRAY_BUFFER, _vertices.Length * Vertex.STRIDE, _vertices, USAGE_PATTERN.GL_STREAM_DRAW);
+
+        _verticesChanged = false;
+    }
+
+    public override void Render(ref Matrix3 projection, ref Matrix3 modelView)
+    {
+        if (W == 0 || H == 0)
+            return;
+
+        var mv = Matrix3.Translate(ref modelView, X, Y);
+
+        if (OpenGL.LastBoundTexture != _texture.ID)
+        {
+            OpenGL32.glBindTexture(TEXTURE_TARGET.GL_TEXTURE_2D, _texture.ID);
+            OpenGL.LastBoundTexture = _texture.ID;
+        }
+
+        BindVertexBuffer();
+
+        if (_verticesChanged)
+        {
+            SetVertices();
+        }
+
+        Shader.Render(ref projection, ref mv, _vertices.Length, PRIMITIVE_TYPE.GL_TRIANGLE_STRIP);
+
+        UnbindVertexBuffer();
+    }
+    
+}
