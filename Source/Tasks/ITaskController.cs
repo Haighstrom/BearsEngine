be3984a0430a6adb0d4657e161ba9c44cd8110b5
Haighstrom <simon.haigh@gmail.com>
2023-10-09 21:54:27 +0100

    Created interface ITaskController

---
+++ Source/Tasks/ITaskController.cs
@@ -0,0 +1,7 @@
+﻿namespace BearsEngine.Tasks;
+
+public interface ITaskController : IAddable, IUpdatable
+{
+    ITask? CurrentTask { get; set; }
+    Func<ITask?>? GetNextTask { get; set; }
+}
--- Source/Tasks/TaskController.cs
+++ Source/Tasks/TaskController.cs
@@ -1,64 +1,62 @@
-﻿namespace BearsEngine.Tasks
+﻿namespace BearsEngine.Tasks;
+
+public class TaskController : AddableBase, ITaskController
 {
-    public class TaskController : AddableBase, IUpdatable
+    public TaskController()
     {
-        public TaskController()
-        {
-            CurrentTask = null;
-            GetNextTask = null;
-        }
-
-        public TaskController(ITask[] tasks)
-        {
-            if (tasks.Length == 0)
-                throw new ArgumentException($"TaskController {this} had an empty list of tasks {tasks} passed to it.");
-
-            for (int i = 0; i < tasks.Length - 1; i++)
-                tasks[i].NextTask = tasks[i + 1];
-
-            CurrentTask = tasks[0];
-        }
-
-        public TaskController(Func<ITask?>? getNextTask = null)
-            : this(null, getNextTask)
-        {
-        }
-
-        public TaskController(ITask? firstTask, Func<ITask?>? getNextTask = null)
-        {
-            CurrentTask = firstTask;
-            GetNextTask = getNextTask;
-        }
-        
-
-        public virtual bool Active { get; set; } = true;
-        public ITask? CurrentTask { get; set; }
-        public Func<ITask?>? GetNextTask { get; set; }
-
-        public virtual void Update(float elapsed)
-        {
-            if (Parent is null)
-                return;
-
-            if (CurrentTask != null && CurrentTask.Active)
-            {
-                CurrentTask.Update(elapsed);
-
-                if (CurrentTask.IsComplete)
-                {
-                    CurrentTask.Complete();
-                    if (Parent is IAddable ie && ie.Parent is null) //kind of a hack?
-                        return;
-                    CurrentTask = CurrentTask.NextTask;
-                }
-            }
-
-            if (Parent is IAddable a && a.Parent is null) //kind of a hack?
-                return;
-
-            if (CurrentTask == null && GetNextTask != null)
-                CurrentTask = GetNextTask();
-        }
-        
+        CurrentTask = null;
+        GetNextTask = null;
     }
+
+    public TaskController(ITask[] tasks)
+    {
+        Ensure.ArgumentCollectionNotNullOrEmpty(tasks, nameof(tasks));
+
+        for (int i = 0; i < tasks.Length - 1; i++)
+            tasks[i].NextTask = tasks[i + 1];
+
+        CurrentTask = tasks[0];
+    }
+
+    public TaskController(Func<ITask?>? getNextTask = null)
+        : this(null, getNextTask)
+    {
+    }
+
+    public TaskController(ITask? firstTask, Func<ITask?>? getNextTask = null)
+    {
+        CurrentTask = firstTask;
+        GetNextTask = getNextTask;
+    }
+
+
+    public virtual bool Active { get; set; } = true;
+    public ITask? CurrentTask { get; set; }
+    public Func<ITask?>? GetNextTask { get; set; }
+
+    public virtual void Update(float elapsed)
+    {
+        if (Parent is null)
+            return;
+
+        if (CurrentTask != null && CurrentTask.Active)
+        {
+            CurrentTask.Update(elapsed);
+
+            if (CurrentTask.IsComplete)
+            {
+                CurrentTask.Complete();
+                if (Parent is IAddable ie && ie.Parent is null) //kind of a hack?
+                    return;
+                CurrentTask = CurrentTask.NextTask;
+            }
+        }
+
+        if (Parent is IAddable a && a.Parent is null) //kind of a hack?
+            return;
+
+        if (CurrentTask == null && GetNextTask != null)
+            CurrentTask = GetNextTask();
+    }
+
 }
