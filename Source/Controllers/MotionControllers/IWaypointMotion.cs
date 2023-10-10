670a23cffcfe513dd26e41b814ad2ca7dfefaf87
Haighstrom <simon.haigh@gmail.com>
2023-10-10 14:21:51 +0100

    Made IWaypointMotion an IAddable

--- Source/Controllers/MotionControllers/IWaypointMotion.cs
+++ Source/Controllers/MotionControllers/IWaypointMotion.cs
@@ -1,21 +1,20 @@
 ﻿using BearsEngine.Source.Controllers.MotionControllers;
 
-namespace BearsEngine.Controllers
+namespace BearsEngine.Controllers;
+
+public interface IWaypointMotion : IAddable
 {
-    public interface IWaypointMotion
-    {
-        Point CurrentPosition { get; set; }
-        bool ReachedDestination { get; }
-        IList<IPosition> Waypoints { get; }
+    Point CurrentPosition { get; set; }
+    bool ReachedDestination { get; }
+    IList<IPosition> Waypoints { get; }
 
-        event EventHandler<EventArgs>? Arrived;
-        event EventHandler<DirectionChangedEventArgs>? DirectionChanged;
+    event EventHandler<EventArgs>? Arrived;
+    event EventHandler<DirectionChangedEventArgs>? DirectionChanged;
 
-        void AddWaypoints(IEnumerable<IPosition> positions);
-        void AddWaypoints(params IPosition[] positions);
-        void ClearWaypoints();
-        IPosition GetNextWaypoint();
-        void SetWaypoints(IEnumerable<IPosition> positions);
-        void SetWaypoints(params IPosition[] positions);
-    }
+    void AddWaypoints(IEnumerable<IPosition> positions);
+    void AddWaypoints(params IPosition[] positions);
+    void ClearWaypoints();
+    IPosition GetNextWaypoint();
+    void SetWaypoints(IEnumerable<IPosition> positions);
+    void SetWaypoints(params IPosition[] positions);
 }
