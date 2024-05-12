12163f43fd44093a2f3219e78ceb056258e397b1
Haighstrom <simon.haigh@gmail.com>
2024-05-12 11:30:36 +0100

    Created UIFactory to start building UI components

---
+++ BearsEngine/Source/UI/UIFactory.cs
@@ -0,0 +1,23 @@
+﻿using BearsEngine.Input;
+
+namespace BearsEngine.UI;
+
+public class UIFactory
+{
+    private readonly IMouse _mouse;
+
+    public UIFactory(IMouse mouse)
+    {
+        _mouse = mouse;
+    }
+
+    public Button CreateButton(float layer, Rect position, string graphic, UITheme theme, string text = "", Action? actionOnClicked = null)
+    {
+        return new Button(_mouse, layer, position, graphic, theme, text, actionOnClicked);
+    }
+
+    public Button CreateButton(float layer, Rect position, Colour graphic, UITheme theme, string text = "", Action? actionOnClicked = null)
+    {
+        return new Button(_mouse, layer, position, graphic, theme, text, actionOnClicked);
+    }
+}
