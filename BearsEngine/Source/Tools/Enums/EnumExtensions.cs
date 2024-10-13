7e30fbbc96820497fa4e9874491f6df00cfb34c6
Haighstrom <simon.haigh@gmail.com>
2024-10-13 12:02:12 +0100

    Added an extention function for incrementing enums myEnum.Next()

---
+++ BearsEngine/Source/Tools/Enums/EnumExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace BearsEngine;
+
+public static class EnumExtensions
+{
+    public static T Next<T>(this T src) where T : Enum
+    {
+        var arr = (T[])Enum.GetValues(src.GetType());
+
+        int j = Array.IndexOf(arr, src) + 1;
+
+        return (arr.Length == j) ? arr[0] : arr[j];
+    }
+}
